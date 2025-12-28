32eee775a577b35224566937f3acd1216e130408
MisteRy Tech <47310669+misteryTech@users.noreply.github.com>
2025-12-29 02:44:59 +0800

    asd
    asd

--- index.php
+++ index.php
@@ -1,18 +1,44 @@
-<?php
-// File to store data
-$filename = "esp32_data.txt";
+#include <WiFi.h>
+#include <WebServer.h>
 
-// Check if ESP32 sent a value via GET
-if (isset($_GET['value'])) {
-    $data = $_GET['value'];
-    file_put_contents($filename, $data); // Save data to file
-    echo "Data received: " . $data;
-} else {
-    // If no data sent, show last value
-    if (file_exists($filename)) {
-        echo "Last ESP32 value: " . file_get_contents($filename);
-    } else {
-        echo "No data received yet";
-    }
+// WiFi credentials
+const char* ssid = "mistery_home";
+const char* password = "aiysfuresaill12";
+
+// Create a web server on port 80
+WebServer server(80);
+
+// Example data to show
+String sensorData = "Hello from ESP32!";
+
+void handleRoot() {
+  String html = "<!DOCTYPE html><html><head><title>ESP32</title></head><body>";
+  html += "<h1>ESP32 Web Server</h1>";
+  html += "<p>Data: " + sensorData + "</p>";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
 }
-?>
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to WiFi!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot); // Root URL
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient(); // Handle client requests
+}
